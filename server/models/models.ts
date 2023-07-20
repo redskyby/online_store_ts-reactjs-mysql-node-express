8066519fe428a56373c2d076455dcd509e2179e1
Docenko Pasha <pashadocenko@gmail.com>
2023-07-20 21:19:32 +0300

    User is initialized.

--- server/models/models.ts
+++ server/models/models.ts
@@ -7,3 +7,28 @@ class User extends Model {
     public password! : number;
     public role! : string;
 }
+
+User.init(
+    {
+        id:{
+            type :DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement:true,
+        },
+        email:{
+            type: DataTypes.STRING,
+            unique:true
+        },
+        password:{
+            type : DataTypes.STRING
+        },
+        role:{
+            type : DataTypes.STRING,
+            defaultValue : "USER"
+        }
+    },
+    {
+        sequelize,
+        modelName: "USER"
+    }
+)
