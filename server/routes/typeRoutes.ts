50100c74c0caaa56523b99c6fdf490d15e6ad1f3
Docenko Pasha <pashadocenko@gmail.com>
2023-08-20 00:00:34 +0300

    brandRoutes.ts, typeRoute.ts ,userRoutes.ts  were initialized.

---
+++ server/routes/typeRoutes.ts
@@ -0,0 +1,7 @@
+import {Router} from 'express';
+const router = Router();
+
+router.post("/", );
+router.get("/", );
+
+export default router;
---
+++ server/routes/userRoutes.ts
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+const router = Router();
+
+router.post("/registration", );
+router.post("/login", );
+router.get("/auth", );
+
+export default router;
