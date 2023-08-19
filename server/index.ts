f5bd720a4fda848a4d7f03f797f9dea1871dd6fe
Docenko Pasha <pashadocenko@gmail.com>
2023-08-19 23:42:45 +0300

    necessary routes ware added.

--- server/index.ts
+++ server/index.ts
@@ -12,13 +12,11 @@ const port: number = parseInt(process.env.PORT!, 10) || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/' , (req, res)=> {
-    res.status(200).json({message: "WORK!!!!"});
-});
 
 const start = async () => {
     try {
         await sequelize.authenticate();
+        console.log("есть подключение!");
         await sequelize.sync();
         app.listen(port, () => {
             console.log(`Server running at http://localhost:${port}`);
