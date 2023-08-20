006fe28aafb8d1ff20aa553e15f7e7a1320f7f7f
Docenko Pasha <pashadocenko@gmail.com>
2023-08-20 23:19:17 +0300

    ErrorHandlingMiddleware.ts was tested. (ok)

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,15 +1,21 @@
-import {Request , Response} from "express";
+import {NextFunction, Request, Response} from "express";
+import ApiError from "../error/ApiError";
 
 class UserController {
-    async registration(req : Request, res: Response){
+    async registration(req: Request, res: Response) {
 
     }
-    async login(req : Request, res: Response){
+
+    async login(req: Request, res: Response) {
 
     }
-    async check(req : Request, res: Response){
-        const query = req.query;
-        res.json(query);
+
+    async check(req: Request, res: Response, next: NextFunction) {
+        const {id} = req.query;
+        if (!id) {
+            return next(ApiError.badRequest("Не задан ID!"));
+        }
+        res.json(id);
     }
 }
 
