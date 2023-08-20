e9f9e4816040b5d19f6c412dd8f080f51de10ef3
Docenko Pasha <pashadocenko@gmail.com>
2023-08-20 22:53:21 +0300

    ApiError.ts was added.

---
+++ server/error/ApiError.ts
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+
+    status: number;
+    message: string;
+
+    constructor(status: number, message: string) {
+        super();
+        this.status = status
+        this.message = message
+    }
+
+    static badRequest(message : string) {
+        return new ApiError(404, message);
+    }
+    static internal(message : string) {
+        return new ApiError(500, message);
+    }
+    static forBidden(message : string) {
+        return new ApiError(403, message);
+    }
+}
