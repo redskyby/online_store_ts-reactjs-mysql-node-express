f01ed2da47cce559e13754360835c3745cf10ac9
Docenko Pasha <pashadocenko@gmail.com>
2023-08-20 23:13:02 +0300

    ErrorHandlingMiddleware.ts was registered.

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import sequelize  from "./db_sequalize";
 import models from "./models/models";
 import cors from 'cors';
 import routes from "./routes/index";
+import errorHandlingMiddleware from "./middleware/ErrorHandlingMiddleware";
 config();
 
 const app: Express = express();
@@ -12,6 +13,9 @@ const port: number = parseInt(process.env.PORT!, 10) || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/api/' , routes);
+//MiddleWare должен быть в самом конце.
+//errorHandlingMiddleware -- замыкающий
+app.use(errorHandlingMiddleware);
 
 const start = async () => {
     try {
--- server/middleware/ErrorHandlingMiddleware.ts
+++ server/middleware/ErrorHandlingMiddleware.ts
@@ -1,7 +1,7 @@
 import ApiError from "../error/ApiError";
 import {Response, Request ,NextFunction} from "express";
 
-export default function (err : Error, req: Request, res :Response, next: NextFunction ){
+export default function (err: Error , req: Request, res :Response, next: NextFunction ){
             if(err instanceof  ApiError){
                return res.status(err.status).json({message: err.message});
             }
