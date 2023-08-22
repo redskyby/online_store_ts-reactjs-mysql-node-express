65bed9460d979a47cf788f11cff6076ba072540f
Docenko Pasha <pashadocenko@gmail.com>
2023-08-22 14:25:19 +0300

    typeController.ts initialization has started.

--- server/controllers/typeController.ts
+++ server/controllers/typeController.ts
@@ -1,10 +1,12 @@
 import {Request, Response} from "express";
-import Type from "../models/models";
+import models from "../models/models";
 import ApiError from "../error/ApiError";
 
 class typeController {
     async create(req: Request, res: Response) {
-
+            const {name} = req.body;
+            const type  = await models.Type.create({name});
+            return res.json(type);
     }
 
     async getAll(req: Request, res: Response) {
