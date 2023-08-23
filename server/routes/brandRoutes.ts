594fd1169786cb376c14c96ce8608eb7a3137e6f
Docenko Pasha <pashadocenko@gmail.com>
2023-08-23 14:04:19 +0300

    Controllers were added the all routers

--- server/routes/brandRoutes.ts
+++ server/routes/brandRoutes.ts
@@ -1,7 +1,8 @@
 import {Router} from 'express';
 const router = Router();
+import brandController from "../controllers/brandController";
 
-router.post("/", );
-router.get("/", );
+router.post("/",brandController.create );
+router.get("/", brandController.getAll);
 
 export default router;
--- server/routes/deviceRoutes.ts
+++ server/routes/deviceRoutes.ts
@@ -1,10 +1,10 @@
 import {Router} from 'express';
 const router = Router();
+import deviceController from "../controllers/deviceController";
 
 
-
-router.post("/", );
-router.get("/", );
-router.get("/:id", );
+router.post("/", deviceController.create);
+router.get("/", deviceController.getAll);
+router.get("/:id", deviceController.getOne);
 
 export default router;
