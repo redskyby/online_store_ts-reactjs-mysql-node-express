b4fe35ac2b3f1c6bc0aa517c2fb256f5354afadd
Docenko Pasha <pashadocenko@gmail.com>
2023-08-26 22:26:16 +0300

    brandController.ts is done.

--- server/controllers/brandController.ts
+++ server/controllers/brandController.ts
@@ -1,12 +1,17 @@
 import {Request, Response} from "express";
+import models from "../models/models";
+import ApiError from "../error/ApiError";
 
 class brandController {
     async create(req: Request, res: Response) {
-
+        const {name} = req.body;
+        const brand = await models.Brand.create({name});
+        return res.json(brand);
     }
 
     async getAll(req: Request, res: Response) {
-
+        const brands = await models.Brand.findAll();
+        return res.json(brands);
     }
 }
 
