d1dbaea6cdf58e3f20a65efb9884e179d17c902d
Docenko Pasha <pashadocenko@gmail.com>
2023-08-26 22:35:12 +0300

    add fileUpLoad

--- server/controllers/deviceController.ts
+++ server/controllers/deviceController.ts
@@ -2,14 +2,15 @@ import {Request, Response} from "express";
 
 class deviceController {
     async create(req: Request, res: Response) {
-
+        const {name, price, brandId , typeId, info} = req.body;
+        const {img} = req.files;
     }
 
     async getAll(req: Request, res: Response) {
 
     }
 
-    async getOne(req: Request, res: Response){
+    async getOne(req: Request, res: Response) {
 
     }
 }
