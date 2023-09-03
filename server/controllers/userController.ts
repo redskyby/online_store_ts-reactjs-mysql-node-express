ed0facd8e178625b80a99e61535862d587243a3e
Docenko Pasha <pashadocenko@gmail.com>
2023-09-03 20:30:16 +0300

    Registration was started initialization. (UserController.ts)

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -3,9 +3,12 @@ import ApiError from "../error/ApiError";
 import models from "../models/models";
 
 class UserController {
-    async registration(req: Request, res: Response) {
+    async registration(req: Request, res: Response, next: NextFunction) {
         const {email, password, role} = req.body;
-
+        if (!email || !password) {
+            return next(ApiError.badRequest("Не коректный email или password!"));
+        }
+        const candidate = await models.User.findOne({where : {email}})
     }
 
     async login(req: Request, res: Response) {
