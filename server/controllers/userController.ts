9e7c7b4d484a4cf479d66b9ee40d5cd90cc0cda2
Docenko Pasha <pashadocenko@gmail.com>
2023-09-03 21:04:07 +0300

    SECRET_KEY was added to .env

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,6 +1,11 @@
 import {NextFunction, Request, Response} from "express";
 import ApiError from "../error/ApiError";
 import models from "../models/models";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {config} from "dotenv";
+
+config();
 
 class UserController {
     async registration(req: Request, res: Response, next: NextFunction) {
@@ -8,7 +13,15 @@ class UserController {
         if (!email || !password) {
             return next(ApiError.badRequest("Не коректный email или password!"));
         }
-        const candidate = await models.User.findOne({where : {email}})
+        const candidate = await models.User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest("Пользователь с такм email существует!"));
+        }
+
+        const hashPassword : string = await bcrypt.hash(password, 5);
+        const user = await models.User.create({email, role, password: hashPassword});
+        const basket = await models.Basket.create({userId: user.id});
+        const jwToken = jwt.sign({id : user.id , email : user.email , role}, process.env.SECRET_KEY!);
     }
 
     async login(req: Request, res: Response) {
