781be6cc1eb728165836291e48ad913874f9fd2b
Docenko Pasha <pashadocenko@gmail.com>
2023-09-08 13:34:26 +0300

    checkRoleMiddleware.ts was initialised.

---
+++ server/middleware/checkRoleMiddleware.ts
@@ -0,0 +1,36 @@
+import {NextFunction, Request, Response} from "express";
+import jwt from "jsonwebtoken";
+import {config} from "dotenv";
+
+config();
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: any;
+        }
+    }
+}
+
+export default function (role: string) {
+    return function (req: Request, res: Response, next: NextFunction) {
+        if (req.method === "OPTIONS") {
+            next();
+        }
+        try {
+            const token: string = req.headers.authorization!.split(' ')[1];
+            if (!token) {
+                return res.status(401).json({message: "Не авторизован!"});
+            }
+            const decoded: string | any = jwt.verify(token, process.env.SECRET_KEY!);
+            if (decoded.role !== role) {
+                return res.status(403).json({message: "Нет доступа!"});
+            }
+            req.user = decoded;
+            next();
+        } catch (e) {
+            res.status(401).json({message: "Не авторизован!"});
+        }
+    }
+}
+
