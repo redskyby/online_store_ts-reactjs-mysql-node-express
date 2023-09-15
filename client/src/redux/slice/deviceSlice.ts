1c334141ea1530ab207f4a1d9a3c50fbc5c71936
Docenko Pasha <pashadocenko@gmail.com>
2023-09-15 22:59:48 +0300

    deviceSlice.ts was initialized.

---
+++ client/src/redux/slice/deviceSlice.ts
@@ -0,0 +1,52 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface Type_Brand {
+    id: number,
+    name: string
+}
+
+interface Device {
+    id: number,
+    name: string,
+    price: number,
+    rating: number,
+    img: string
+}
+
+const initialState = {
+    types: [
+        {id: 1, name: 'Холодильники'},
+        {id: 2, name: 'Смартфоны'}
+    ],
+    brands: [
+        {id: 1, name: 'Samsung'},
+        {id: 2, name: 'Apple'}
+    ],
+    device: [
+        {id: 1, name: 'Iphone 12 pro', price: 25000, rating: 5, img: "img"},
+        {id: 2, name: 'Iphone 12 pro', price: 25000, rating: 5, img: "img"},
+    ]
+}
+
+const deviceSlice = createSlice({
+    name: 'device',
+    initialState,
+    reducers: {
+        SET_TYPES: (state, action: PayloadAction<Type_Brand>) => {
+            state.types.push(action.payload);
+        },
+        SET_BRANDS: (state, action: PayloadAction<Type_Brand>) => {
+            state.brands.push(action.payload);
+        },
+        SET_DEVICE: (state, action: PayloadAction<Device>) => {
+            state.device.push(action.payload);
+        },
+    }
+})
+
+export default deviceSlice.reducer;
+export const {
+    SET_TYPES,
+    SET_BRANDS,
+    SET_DEVICE
+} = deviceSlice.actions;
