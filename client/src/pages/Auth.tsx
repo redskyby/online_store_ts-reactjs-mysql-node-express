3a813d1043e5fc4e57cd735307c60ee9b3497e70
Docenko Pasha <pashadocenko@gmail.com>
2023-09-16 23:56:47 +0300

    Auth.tsx was started to init.

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 import {REGISTRATION_ROUTE} from '../utils/const';
 
@@ -22,15 +22,19 @@ const Auth = () => {
                         className={'mt-3'}
                         placeholder={"Введите ваш пароль..."}
                     />
-                    <Row className="mt-3 pl-3 pr-3">
-                        <div>
-                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
-                        </div>
-                        <Button
-                            variant={"outline-success"}
-                            className={"mt-3 align-self-end"}
-                        >Войти
-                        </Button>
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        <Col  className={"d-flex  justify-content-end align-items-center"}>
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+                            </div>
+                        </Col>
+                        <Col  className={"d-flex  justify-content-end"}>
+                            <Button
+                                variant={"outline-success"}
+
+                            >Войти
+                            </Button>
+                        </Col>
                     </Row>
                 </Form>
             </Card>
