5add891bd7b03b85b07abee54c1571e382d74580
Docenko Pasha <pashadocenko@gmail.com>
2023-09-20 14:02:03 +0300

    DevicePage.tsx was started to init.

--- client/src/pages/DevicePage.tsx
+++ client/src/pages/DevicePage.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import {Col, Container} from 'react-bootstrap';
+import {Col, Container, Image, Row} from 'react-bootstrap';
+import BigStar from '../assets/bigStar.png';
 
 const DevicePage = () => {
-    return (
-        <Container>
-            <Col md={4}>
+    const device = {id: 1, name: 'Iphone 12 pro', price: 25000, rating: 5, img: "img"};
 
+    return (
+        <Container className={"mt-3"}>
+            <Col md={4}>
+                <Image width={300} height={300} src={device.img}/>
             </Col>
             <Col md={4}>
+                <Row className={'d-flex flex-column align-items-center '}>
+                    <h2 style={{textAlign: "center"}}>{device.name}</h2>
+                    <div
+                        className={"d-flex align-items-center justify-content-center"}
+                        style={{
+                            background: `url(${BigStar}) no-repeat center center`,
+                            width: 240, height: 240,
+                            backgroundSize: "cover",
+                            fontSize: 64
+                        }}
+                    >
 
+                        {device.rating}
+                    </div>
+                </Row>
             </Col>
             <Col md={4}>
 
