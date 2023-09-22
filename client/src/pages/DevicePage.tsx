2087a68a7f34159e60dd92d527b7bea6a6cb8c3c
Docenko Pasha <pashadocenko@gmail.com>
2023-09-23 02:10:19 +0300

    DevicePage.tsx was started to init.

--- client/src/pages/DevicePage.tsx
+++ client/src/pages/DevicePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Col, Container, Image, Row} from 'react-bootstrap';
+import {Button, Card, Col, Container, Image, Row} from 'react-bootstrap';
 import BigStar from '../assets/bigStar.png';
 
 const DevicePage = () => {
@@ -29,7 +29,15 @@ const DevicePage = () => {
                     </Row>
                 </Col>
                 <Col md={4}>
-
+                    <Card
+                        className={"d-flex flex-column align-items-center justify-content-around"}
+                        style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
+                    >
+                        <h3>От: {device.price} руб.</h3>
+                        <Button
+                            variant={"outline-dark"}
+                        >Добавить в корзину</Button>
+                    </Card>
                 </Col>
             </Row>
         </Container>
