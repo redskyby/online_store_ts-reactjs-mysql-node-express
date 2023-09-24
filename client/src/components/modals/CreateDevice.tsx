834ea2d0bb8efb89f1a722d23223766e2d3361ca
Docenko Pasha <pashadocenko@gmail.com>
2023-09-24 20:56:00 +0300

    Modals were started to init.

--- client/src/components/modals/CreateDevice.tsx
+++ client/src/components/modals/CreateDevice.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import {Button, Form, FormControl, Modal, ModalBody, ModalFooter, ModalHeader, ModalTitle} from "react-bootstrap";
 
-const CreateDevice = () => {
+const CreateDevice = ({show, onHide}) => {
     return (
-        <div>
-            
-        </div>
+        <Modal
+            show={show}
+            onHide={onHide}
+            size={"lg"}
+        >
+            <ModalHeader closeButton>
+                <ModalTitle>
+                    Добавить тип
+                </ModalTitle>
+            </ModalHeader>
+            <ModalBody>
+                <Form>
+                    <FormControl placeholder={"Введите название брэнда..."}/>
+                </Form>
+            </ModalBody>
+            <ModalFooter>
+                <Button
+                    variant={"outline-danger"}
+                    onClick={onHide}
+                >
+                    Закрыть
+                </Button>
+                <Button
+                    variant={"outline-success"}
+                    onClick={onHide}
+                >
+                    Добавить
+                </Button>
+            </ModalFooter>
+        </Modal>
     );
 };
 
--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Button, Container} from "react-bootstrap";
+import CreateBrand from "../components/modals/CreateBrand";
+import CreateDevice from "../components/modals/CreateDevice";
+import CreateType from "../components/modals/CreateType";
 
 const Admin = () => {
+
+
     return (
         <Container className={"d-flex flex-column"}>
             <Button variant={"outline-dark"} className={"mt-4 p-2"}>Добавить тип</Button>
             <Button variant={"outline-dark"} className={"mt-4 p-2"}>Добавить бренд</Button>
             <Button variant={"outline-dark"} className={"mt-4 p-2"}>Добавить устройство</Button>
+            <CreateBrand/>
+            <CreateDevice/>
+            <CreateType show={true} onHide={false}/>
         </Container>
     );
 };
