5cc03a769d09aae9b9684d5d7775ed1668e99e20
Docenko Pasha <pashadocenko@gmail.com>
2023-09-24 20:31:51 +0300

    Admin.tsx was started to init.

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Button, Container} from "react-bootstrap";
 
 const Admin = () => {
     return (
-        <div>
-            admin
-        </div>
+        <Container className={"d-flex flex-column"}>
+            <Button variant={"outline-dark"} className={"mt-4 p-2"}>Добавить тип</Button>
+            <Button variant={"outline-dark"} className={"mt-4 p-2"}>Добавить бренд</Button>
+            <Button variant={"outline-dark"} className={"mt-4 p-2"}>Добавить устройство</Button>
+        </Container>
     );
 };
 
