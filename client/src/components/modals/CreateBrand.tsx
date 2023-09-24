06e46cbf0ebf708903adf916015e2ab08d8ddfc3
Docenko Pasha <pashadocenko@gmail.com>
2023-09-24 20:34:28 +0300

    Modals were added.

---
+++ client/src/components/modals/CreateBrand.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateBrand = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CreateBrand;
---
+++ client/src/components/modals/CreateType.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateType = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CreateType;
