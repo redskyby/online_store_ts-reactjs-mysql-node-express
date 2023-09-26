75ec652672ed0fc468749dba41923fecda88c8fb
Docenko Pasha <pashadocenko@gmail.com>
2023-09-26 22:11:01 +0300

    Connection client with server was started to init.

---
+++ client/src/http/index.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const $host = axios.create({
+    baseURL: process.env.REACT_APP_API_URL
+});
+
+const $authHost = axios.create({
+    baseURL: process.env.REACT_APP_API_URL
+});
