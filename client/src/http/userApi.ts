189bc48ac618fc8e06cf2610941cc095ce6eab87
Docenko Pasha <pashadocenko@gmail.com>
2023-09-29 23:41:32 +0300

    The token was added to localStorage.

--- client/src/http/userApi.ts
+++ client/src/http/userApi.ts
@@ -3,10 +3,12 @@ import jwtDecode from "jwt-decode";
 
 export const registration = async (email: string, password: string) => {
     const {data} = await $host.post('api/user/registration', {email, password, role: 'ADMIN'});
+    localStorage.setItem('token' , data.token);
     return jwtDecode(data.token);
 }
 export const login = async (email: string, password: string) => {
     const {data}  = await $host.post('api/user/login', {email, password});
+    localStorage.setItem('token' , data.token);
     return jwtDecode(data.token);
 }
 export const check = async () => {
