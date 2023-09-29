f273f5f927a43bd66380b9872579ff7d81babd27
Docenko Pasha <pashadocenko@gmail.com>
2023-09-29 23:06:28 +0300

    jwt decoded works.

--- client/src/http/userApi.ts
+++ client/src/http/userApi.ts
@@ -1,12 +1,13 @@
 import {$authHost, $host} from "./index";
+import jwtDecode from "jwt-decode";
 
 export const registration = async (email: string, password: string) => {
-    const response = await $host.post('api/user/registration', {email, password, role: 'ADMIN'});
-    return response;
+    const {data} = await $host.post('api/user/registration', {email, password, role: 'ADMIN'});
+    return jwtDecode(data.token);
 }
 export const login = async (email: string, password: string) => {
-    const response = await $host.post('api/user/login', {email, password, role: "ADMIN"});
-    return response;
+    const {data}  = await $host.post('api/user/login', {email, password});
+    return jwtDecode(data.token);
 }
 export const check = async () => {
     const response = await $host.get('api/user/auth');
