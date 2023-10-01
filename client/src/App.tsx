02ed6e0bea296bb040944dcdfca1ae19355155f4
Docenko Pasha <pashadocenko@gmail.com>
2023-10-01 18:02:24 +0300

    App.css was deleted.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
