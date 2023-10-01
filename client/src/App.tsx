77471b027b697a6f71d6bd2055ac5089546c194d
Docenko Pasha <pashadocenko@gmail.com>
2023-10-01 22:27:41 +0300

    The jwt token exists for 2 hours.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,15 @@ import React, {useEffect, useState} from 'react';
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import UserApi from "./http/userApi";
 import {IS_SET_AUTH} from "./redux/slice/isAuthSlice";
 import {RingLoader} from "react-spinners";
 import {Container} from "react-bootstrap";
+import {RootState} from "./redux";
 
 function App() {
+    const isAuth: boolean = useSelector((state: RootState) => state.isAuthToolkit.isAuth);
     const [loading, setLoading] = useState(true);
     const dispatch = useDispatch();
 
@@ -20,7 +22,7 @@ function App() {
         }else{
             setLoading(false);
         }
-    }, [])
+    }, [isAuth])
 
     if (loading) {
         return (
