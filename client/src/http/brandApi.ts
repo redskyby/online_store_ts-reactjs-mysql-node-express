2dd3326b7f8f99b507f311c9f746fc3b68c62827
Docenko Pasha <pashadocenko@gmail.com>
2023-10-04 01:15:07 +0300

    brandApi.ts was done.

--- client/src/http/brandApi.ts
+++ client/src/http/brandApi.ts
@@ -1,5 +1,15 @@
-class BrandApi {
+import {$authHost, $host} from "./index";
 
+class BrandApi {
+    public async createBrand(brand: string) {
+        const {data} = await $authHost.post('api/brand', brand);
+        return data;
+    }
+
+    public async fetchBrands() {
+        const {data} = await $host.get('api/brand');
+        return data;
+    }
 }
 
 export default new BrandApi();
