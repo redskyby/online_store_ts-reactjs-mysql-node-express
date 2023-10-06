32ed93c4027771d3915f9c566b9b6dbd8af6ebe5
Docenko Pasha <pashadocenko@gmail.com>
2023-10-06 13:52:29 +0300

    deviceApi.ts was done.

--- client/src/http/deviceApi.ts
+++ client/src/http/deviceApi.ts
@@ -1,5 +1,19 @@
-class DeviceApi {
+import {$authHost, $host} from "./index";
 
+class DeviceApi {
+    public async createDevice(type: string) {
+        const {data} = await $authHost.post('api/device', type);
+        return data;
+    }
+
+    public async fetchDevices() {
+        const {data} = await $host.get('api/device');
+        return data;
+    }
+    public async fetchOneDevice(id:number) {
+        const {data} = await $host.get('api/device/' + id);
+        return data;
+    }
 }
 
 export default new DeviceApi();
