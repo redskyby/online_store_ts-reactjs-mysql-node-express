c6134410b0cd00ab98b4e261eb530c7c2e30c74c
Docenko Pasha <pashadocenko@gmail.com>
2023-10-14 17:16:51 +0300

    Pages.tsx was changed name to Pagination.tsx.

--- client/src/components/Pagination.tsx
+++ client/src/components/Pagination.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Pages = () => {
+const Pagination = () => {
     return (
         <div>
 
@@ -8,4 +8,4 @@ const Pages = () => {
     );
 };
 
-export default Pages;
+export default Pagination;
